0db14009874511970bfa1f87b289ad92cdd0d6e3
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-08-25 09:29:10 +0800

    Create index.js
    Initial Commit for index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const applicationRun = require('electron')
+const app = applicationRun.app
+const BrowserWindow = applicationRun.BrowserWindow
+
+function initialNewWindow()
+{
+    const mainWindow = new BrowserWindow({minWidth: 1920, minHeight: 1080, Width: 1280, Height: 720, transparent: true, frame: false, webPreferences: {nodeIntegration: true}})
+    mainWindow.loadFile('index.html')
+    mainWindow.webContents.openDevTools()
+    mainWindow.setPosition(0,540)
+
+    mainWindow.webContents.BrowserWindow
+    mainWindow.applicationRun
+    mainWindow.setIgnoreMouseEvents(true)
+}
+
+app.whenReady().then(() => {initialNewWindow()})
