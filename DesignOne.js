0e1710095659dff496a0b9a0f650bb5b8911b895
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-09-02 00:47:01 +0800

    Create DesignOne.js
    Initial Commit for DesignOne.js

---
+++ DesignOne.js
@@ -0,0 +1,18 @@
+/* Get electron and fs functions */
+const fsSource = require('fs')
+const electronSource = require('electron')
+/* Get electron and fs functions */
+
+/* Get a Message Window from the Remote Module */
+// const MessageWindow = electronSource.remote
+const {MessageWindow} = electronSource.remote
+/* Get a Message Window from the Remote Module */
+
+// from the document module of node
+// getElementById: Returns a reference to the first object with the specified value of the ID or NAME attribute.
+// Element.addEventListener
+document.getElementById("FirstClickArea").addEventListener("FirstClick", () =>
+{
+    const LocalMessage = "*** Beginning of the Local Message ***";
+    MessageWindow.showSaveDialog(null)
+}
