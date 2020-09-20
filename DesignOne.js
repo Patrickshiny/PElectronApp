6f6c9fa243b514c01fea2ec55bc717cde2779719
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-09-21 06:13:20 +0800

    Update DesignOne.js

--- DesignOne.js
+++ DesignOne.js
@@ -5,18 +5,32 @@ const electronSource = require('electron')
 
 /* Get a Message Window from the Remote Module */
 // const MessageWindow = electronSource.remote
-const {MessageWindow} = electronSource.remote
+const { MessageWindow } = electronSource.remote
 /* Get a Message Window from the Remote Module */
 
 // from the document module of node
 // getElementById: Returns a reference to the first object with the specified value of the ID or NAME attribute.
 // Element.addEventListener
-document.getElementById("FirstClickArea").addEventListener("FirstClick", () =>
-{
-    const LocalMessage = "*** Beginning of the Local Message ***";
-    MessageWindow.showSaveDialog.LocalMessage
-    fsSource.writeFileSync()
-    fsSource.writeSync()
-    fsSource.writevSync()
-    fsSource.WriteStream
-}
+document.getElementById("FirstClickArea").addEventListener
+// ("FirstClick", () => 
+("click", () => 
+    {
+        const LocalMessage = "*** Beginning of the Local Message ***";
+        // MessageWindow.showSaveDialog.LocalMessage
+        // fsSource.writeFileSync()
+        // fsSource.writeSync()
+        // fsSource.writevSync()
+        // fsSource.WriteStream
+        MessageWindow.showSaveDialog(
+            filename=>
+            {
+                fsSource.writeFileSync(
+                    filename + ".txt", data, "utf-8", () =>
+                    {
+                        console.log("Saving the File");
+                    }
+                );
+            }
+        );
+    }
+);
