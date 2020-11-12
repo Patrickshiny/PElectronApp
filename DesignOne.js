a5219d3643e6bb0a46f65d6596219f0dfdbbd603
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-11-12 23:51:26 +0800

    Update DesignOne.js

--- DesignOne.js
+++ DesignOne.js
@@ -1,47 +1,72 @@
 /* Get electron and fs functions */
 const fsSource = require('fs')
-const electronSource = require('electron')
+// const electronSource = require('electron')
+// const { BrowserWindow, CurrentWindow, MessageWindow } = require('electron');
+const { BrowserWindow, CurrentWindow} = require('electron');
+const {MessageWindow} = require('electron').remote;
+
+const { dialog } = BrowserWindow.dialog
+
+// BrowserWindow.showSaveDialog
+// CurrentWindow.showSaveDialog
+// MessageWindow.showSaveDialog
+// MessageWindow.showSaveDialog
 /* Get electron and fs functions */
 
 /* Get a Message Window from the Remote Module */
 // const MessageWindow = electronSource.remote
-const { MessageWindow } = electronSource.remote
+// const { MessageWindow } = electronSource.remote
+// const { eleDialog } = electronSource.remote.dialog
+// const {CurrentWindow} = CurrentWindow.showSaveDialog
+// BrowserWindow.getCurrentWindow()
 /* Get a Message Window from the Remote Module */
 
 // from the document module of node
 // getElementById: Returns a reference to the first object with the specified value of the ID or NAME attribute.
 // Element.addEventListener
+
+
 document.getElementById("FirstClickArea").addEventListener
 // ("FirstClick", () => 
+// debugger;
 ("click", () => 
     {
-        const LocalMessage = "*** Beginning of the Local Message ***";
-        console.log(LocalMessage);
+        // const LocalMessage = "*** Beginning of the Local Message ***";
+        const FirstClickAreaMsg = "[FirstClickArea Clicked]";
+        console.log(FirstClickAreaMsg);
+        // console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
+        console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
         // MessageWindow.showSaveDialog.LocalMessage
         // fsSource.writeFileSync()
         // fsSource.writeSync()
         // fsSource.writevSync()
         // fsSource.WriteStream
+        // MessageWindow.showSaveDialog(
+            // eleDialog.showSaveDialog(
+        // CurrentWindow.CurrentWindow
+        // CurrentWindow.MessageWindow.showSaveDialog(
         MessageWindow.showSaveDialog(
-            filename=>
-            {
-                fsSource.writeFileSync(
-                    filename + ".txt", data, "utf-8", () =>
-                    {
-                        console.log("Saving the File");
-                    }
-                );
-            }
+            function (filename) {
+                    fsSource.writeFileSync(
+                        filename + ".txt", data, "utf-8", () => {
+                            console.log("Saving the File");
+                        }
+                    );
+                }
         );
     }
 );
 
-CurrentWindow = electronSource.getCurrentWindow()
-electronSource.ClientRequest
-querySelector()
+// const {CurrentWindow} = electronSource.getCurrentWindow()
+// electronSource.ClientRequest
+
+
 document.getElementById("TurnOffApplication").addEventListener
 ("click", () =>
 {
-    CurrentWindow.Close()
+    const TurnOffApplicationMsg = "[TurnOffApplicationMsg Clicked]";
+     console.log(TurnOffApplicationMsg);
+    // CurrentWindow.Close()
+    CurrentWindow.Closing()
 }
 );
