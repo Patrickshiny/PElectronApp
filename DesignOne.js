1b78ec74326fa8a27ee182b3a86d9f31f79b6ae9
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2021-02-07 21:36:57 +0800

    [210207]

--- DesignOne.js
+++ DesignOne.js
@@ -1,18 +1,18 @@
 /* Get electron and fs functions */
-const fsSource = require('fs')
-const electronSource = require('electron')
-const { BrowserWindow, CurrentWindow, MessageWindow } = require('electron');
-const { BrowserWindow, CurrentWindow} = require('electron');
-
+const fsSource = require('fs');
+// const electronSource = require('electron')
+// const { BrowserWindow, CurrentWindow, MessageWindow } = require('electron');
+// const { BrowserWindow, CurrentWindow } = require('electron')
 /* Original MessageWindow */
 // const {MessageWindow} = require('electron').remote;
 // const MessageWindow = electronSource.remote
 // const { MessageWindow } = electronSource.remote
 /* Original MessageWindow */
-const {remote} = require('electron');
-const {MessageWindow} = remote;
+// const {remote} = require('electron');
+// const {MessageWindow} = remote;
+const { BrowserWindow, CurrentWindow, MessageWindow } = require('electron').remote;
 
-const { dialog } = MessageWindow.dialog
+// const { dialog } = MessageWindow.dialog
 
 // BrowserWindow.showSaveDialog
 // CurrentWindow.showSaveDialog
@@ -25,14 +25,13 @@ const { dialog } = MessageWindow.dialog
 /* Original eleDialog */
 // const { eleDialog } = electronSource.remote.dialog
 /* Original eleDialog */
-
 // const { eleDialog } = remote.dialog
-const { eleDialog } = require("electron").remote;
+const { dialog, eleDialog } = require("electron").remote;
 const fs = require('fs');
 
-var FileSavingPath = eleDialog.showSaveDialog({});
-var FileOpenPath = eleDialog.showOpenDialog({});
-eleDialog.properties
+// var FileSavingPath = eleDialog.showSaveDialog({});
+// var FileOpenPath = eleDialog.showOpenDialog({});
+// eleDialog.properties
 
 // const { CurrentWindow, ChildWindow } = CurrentWindow.showSaveDialog
 // const { ChildWindow } = CurrentWindow.showSaveDialog
@@ -52,21 +51,37 @@ document.getElementById("FirstClickArea").addEventListener
 // debugger;
 ("click", () => 
     {
-        const LocalMessage = "*** Beginning of the Local Message ***";
+        // const LocalMessage = "*** Beginning of the Local Message ***";
         const FirstClickAreaMsg = "[FirstClickArea Clicked]";
         console.log(FirstClickAreaMsg);
         // console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
         console.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
-        MessageWindow.showSaveDialog.LocalMessage
+        // MessageWindow.showSaveDialog.LocalMessage
         // fsSource.writeFileSync()
-        fsSource.writeSync()
-        fsSource.writevSync()
-        fsSource.WriteStream
+        // fsSource.writeSync()
+        // fsSource.writevSync()
+        // fsSource.WriteStream
         // MessageWindow.showSaveDialog(
             // eleDialog.showSaveDialog(
         // CurrentWindow.CurrentWindow
         // CurrentWindow.MessageWindow.showSaveDialog(
-        MessageWindow.showMessageBox()
+
+        /* electron showMessageBox */
+        const options = {
+            type: 'question',
+            buttons: ['Cancel', 'Yes, please', 'No, thanks'],
+            defaultId: 2,
+            title: 'Question',
+            message: 'Do you want to do this?',
+            detail: 'It does not really matter',
+            checkboxLabel: 'Remember my answer',
+            checkboxChecked: true,
+          };
+        
+        MessageWindow.showMessageBox(null);
+        /* electron showMessageBox */
+
+        /* electron showSaveDialog */  
         MessageWindow.showSaveDialog(
             function (filename) {
                     fsSource.writeFileSync(
@@ -76,21 +91,22 @@ document.getElementById("FirstClickArea").addEventListener
                     );
                 }
         );
+        /* electron showSaveDialog */    
+
     }
 );
 
-const {CurrentWindow} = electronSource.getCurrentWindow()
-electronSource.ClientRequest
+// const {CurrentWindow} = electronSource.getCurrentWindow()
+// electronSource.ClientRequest
 
-document.getAnimations()
-DocumentType.properties.remote
+// document.getAnimations()
+// DocumentType.properties.remote
 document.getElementById("TurnOffApplication").addEventListener
 ("click", () =>
 {
     const TurnOffApplicationMsg = "[TurnOffApplicationMsg Clicked]";
-    const TurnOfFPopupMsg = "[Turned Off]";
-    console.log(TurnOffApplicationMsg);
-    CurrentWindow.Close()
+     console.log(TurnOffApplicationMsg);
+    // CurrentWindow.Close()
     CurrentWindow.Closing()
 }
 );
